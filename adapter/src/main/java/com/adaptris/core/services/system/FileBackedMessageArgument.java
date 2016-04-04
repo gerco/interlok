82026bcd634c18262d7242e375dcfb7a9013d2cb
Gerco Dries <gerco@gdries.nl>
2016-04-04 16:10:13 -0400

    Add XStreamAlias for file-backed-message-argument

--- adapter/src/main/java/com/adaptris/core/services/system/FileBackedMessageArgument.java
+++ adapter/src/main/java/com/adaptris/core/services/system/FileBackedMessageArgument.java
@@ -3,7 +3,9 @@ package com.adaptris.core.services.system;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.ServiceException;
 import com.adaptris.core.lms.FileBackedMessage;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
+@XStreamAlias("system-command-file-backed-message-argument")
 public class FileBackedMessageArgument implements CommandArgument {
 
   @Override
